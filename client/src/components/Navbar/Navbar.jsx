d9f94e4da2fc89680b3fec7999fa86f78ccf85e1
brooke <brookewilchenski@outlook.com>
2025-04-07 09:34:12 -0700

    navbar

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './Navbar.css'
+import {useNavigate} from 'react-router-dom';
+import Hamburger from "hamburger-react"
+
+ export default function Navbar(){
+  const navigate = useNavigate();
+  const [isOpen, setOpen] = useState(false)
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <>
+      
+        <nav className="navbar">
+          <h1 className='navHeader'>BulletX</h1>
+
+          {isOpen && (
+          <ul>
+                <li><button className='nav-btn' onClick={() => handleNavigation('/home/:id')}>Home</button></li>
+                <li><button className='nav-btn' onClick={() => handleNavigation('/profile/:id')}>Profile</button></li>
+                <li><button className='nav-btn' onClick={() => handleNavigation('/activity/:id')}>Activity</button></li>
+                <li><button className='nav-btn' onClick={() => handleNavigation('/bullets/:id')}>Bullets</button></li>
+                <li><button className='nav-btn' onClick={() => handleNavigation('/upcoming/:id')}>Upcoming</button></li>
+                <li><button className='nav-btn' onClick={() => handleNavigation('/subordinates/:id')}>Subordinates</button></li>
+                <li><button >Log Out</button></li>
+                </ul>
+          )}
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+        </nav>
+    </>
+  )
+}
+
