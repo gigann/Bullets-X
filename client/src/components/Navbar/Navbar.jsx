f7474ac16b1fe87df7998197d581cff9cf2e5e62
DamonHayes <Hayes.Damon10@gmail.com>
2025-04-08 14:49:48 -0400

    fixed nav bar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -24,12 +24,12 @@ import { useLocalStorage } from '@uidotdev/usehooks';
           </Link>
           {isOpen && (
           <ul>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/home/:id')}>Home</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/profile/:id')}>Profile</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/activity/:id')}>Activity</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/bullets/:id')}>Bullets</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/upcoming/:id')}>Upcoming</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/subordinates/:id')}>Subordinates</button></li>
+                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation(loggedIn ? `/home/${loggedIn.id}` : "")}>Home</button></li>
+                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation(loggedIn ? `/profile/${loggedIn.id}` : "")}>Profile</button></li>
+                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation(loggedIn ? `/activity/${loggedIn.id}` : "")}>Activity</button></li>
+                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation(loggedIn ? `/bullets/${loggedIn.id}` : "")}>Bullets</button></li>
+                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation(loggedIn ? `/upcoming/${loggedIn.id}` : "")}>Upcoming</button></li>
+                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation(loggedIn ? `/subordinates/${loggedIn.id}` : "")}>Subordinates</button></li>
                 <li hidden={!loggedIn} ><button onClick={() => {setLoggedIn(), navigate('/')}}>Log Out</button></li>
                 <li hidden={loggedIn}><button className='nav-btn' onClick={() => handleNavigation('/')}>Log In</button></li>
           </ul>
