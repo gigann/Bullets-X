3fef142cb3587e79b713f26da65422e7913efafd
ashdrey1110 <ashrey1110@gmail.com>
2025-04-08 12:32:50 -0700

    added unit table to db

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import './Navbar.css'
-import {useNavigate, Link} from 'react-router-dom';
-import Hamburger from "hamburger-react"
-import { useLocalStorage } from '@uidotdev/usehooks';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { useNavigate, Link } from "react-router-dom";
+import Hamburger from "hamburger-react";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
- export default function Navbar(){
+export default function Navbar() {
   const navigate = useNavigate();
-  const [isOpen, setOpen] = useState(false)
-  const [loggedIn, setLoggedIn] = useLocalStorage('loggedIn')
+  const [isOpen, setOpen] = useState(false);
+  const [loggedIn, setLoggedIn] = useLocalStorage("loggedIn");
 
   const handleNavigation = (path) => {
     navigate(path);
@@ -15,28 +15,78 @@ import { useLocalStorage } from '@uidotdev/usehooks';
 
   return (
     <>
-
-        <nav className="navbar">
-          <Link to = "/home" >
-            <h1 className='navHeader'>
-              BulletX
-            </h1>
-          </Link>
-          {isOpen && (
+      <nav className="navbar">
+        <Link to="/home">
+          <h1 className="navHeader">BulletsX</h1>
+        </Link>
+        {isOpen && (
           <ul>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/home/:id')}>Home</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/profile/:id')}>Profile</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/activity/:id')}>Activity</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/bullets/:id')}>Bullets</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/upcoming/:id')}>Upcoming</button></li>
-                <li hidden={!loggedIn} ><button className='nav-btn' onClick={() => handleNavigation('/subordinates/:id')}>Subordinates</button></li>
-                <li hidden={!loggedIn} ><button onClick={() => {setLoggedIn(), navigate('/')}}>Log Out</button></li>
-                <li hidden={loggedIn}><button className='nav-btn' onClick={() => handleNavigation('/')}>Log In</button></li>
+            <li hidden={!loggedIn}>
+              <button
+                className="nav-btn"
+                onClick={() => handleNavigation("/home/:id")}
+              >
+                Home
+              </button>
+            </li>
+            <li hidden={!loggedIn}>
+              <button
+                className="nav-btn"
+                onClick={() => handleNavigation("/profile/:id")}
+              >
+                Profile
+              </button>
+            </li>
+            <li hidden={!loggedIn}>
+              <button
+                className="nav-btn"
+                onClick={() => handleNavigation("/activity/:id")}
+              >
+                Activity
+              </button>
+            </li>
+            <li hidden={!loggedIn}>
+              <button
+                className="nav-btn"
+                onClick={() => handleNavigation("/bullets/:id")}
+              >
+                Bullets
+              </button>
+            </li>
+            <li hidden={!loggedIn}>
+              <button
+                className="nav-btn"
+                onClick={() => handleNavigation("/upcoming/:id")}
+              >
+                Upcoming
+              </button>
+            </li>
+            <li hidden={!loggedIn}>
+              <button
+                className="nav-btn"
+                onClick={() => handleNavigation("/subordinates/:id")}
+              >
+                Subordinates
+              </button>
+            </li>
+            <li hidden={!loggedIn}>
+              <button
+                onClick={() => {
+                  setLoggedIn(), navigate("/");
+                }}
+              >
+                Log Out
+              </button>
+            </li>
+            <li hidden={loggedIn}>
+              <button className="nav-btn" onClick={() => handleNavigation("/")}>
+                Log In
+              </button>
+            </li>
           </ul>
-          )}
-          <Hamburger toggled={isOpen} toggle={setOpen} />
-        </nav>
+        )}
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+      </nav>
     </>
-  )
+  );
 }
-
