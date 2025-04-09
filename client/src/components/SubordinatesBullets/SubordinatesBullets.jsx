658094b61a3c9da44f7b64b170bc9b699347206f
Ethan <ethanadiem@gmail.com>
2025-04-09 14:52:32 -0600

    added link to name and award subordinates page

--- client/src/components/SubordinatesBullets/SubordinatesBullets.jsx
+++ client/src/components/SubordinatesBullets/SubordinatesBullets.jsx
@@ -4,6 +4,27 @@ import { useLocalStorage } from "@uidotdev/usehooks";
 
 export default function SubordinatesBullets() {
 
+  // useEffect(() => {
+  //   if (!userID) {
+  //     setLoading(false);
+  //     setError("User not logged in.");
+  //     return;
+  //   }
+
+  //       fetch(`http://localhost:3001/users/supervisor/${userID}`, )
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         console.log("Fetched items data:", data);
+  //         setSubordinateData(data)
+  //         setLoading(false);
+  //       })
+  //       .catch((error) => {
+  //         setLoading(false);
+  //         setError(error.message);
+  //         console.error('Error fetching data:', error);
+  //     });
+  // }, [userID]);
+
 return (
 <>
     <div>Hello</div>
