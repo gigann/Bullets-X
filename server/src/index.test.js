f7ce7a90718872cb116ff0800b99642bf3526d55
gigann <issacgann@gmail.com>
2025-04-14 18:33:58 +0200

    basic server-side test

--- server/src/index.test.js
+++ server/src/index.test.js
@@ -1,12 +1,12 @@
 // WIP
 const request = require('supertest');
-import { app } from './index';
+const { app } = require('./index');
 
 describe('GET /', () => {
-  test('should return "Server is up and running".', (done) => {
-    request(app)
-      .get('/')
-      .expect('Content-Type', /json/)
+   test('should return "Server is up and running".', (done) => {
+     request(app)
+       .get('/')
+       .expect('Content-Type', 'text/html; charset=utf-8')
       .expect('Server is up and running.')
       .end((err, res) => {
         if (err) throw err;
