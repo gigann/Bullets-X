15148da8fa576b49c36c0e65db8c0c4735f0f648
gigann <issacgann@gmail.com>
2025-04-14 19:04:43 +0200

    more server-side test

--- server/src/index.test.js
+++ server/src/index.test.js
@@ -6,7 +6,7 @@ describe('GET /', () => {
    test('should return "Server is up and running".', (done) => {
      request(app)
        .get('/')
-       .expect('Content-Type', 'text/html; charset=utf-8')
+       .expect('Content-Type', /text/)
       .expect('Server is up and running.')
       .end((err, res) => {
         if (err) throw err;
