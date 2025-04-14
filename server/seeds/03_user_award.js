41dbb62855246b9aba30cbb6fca32e1d355023cd
Ethan <ethanadiem@gmail.com>
2025-04-14 14:22:46 -0600

    fixed subordinate data

--- server/seeds/03_user_award.js
+++ server/seeds/03_user_award.js
@@ -11,18 +11,18 @@ exports.seed = async function (knex) {
     { user_id: 1, award_id: 1, status: "Drafting", drafting: true },
     { user_id: 1, award_id: 4, status: "Supervisor Review", drafting: false },
 
-    { user_id: 2, award_id: 1, status: "Supervisor Review", drafting: false },
+    { user_id: 2, award_id: 1, status: "Supervisor Review", drafting: true },
     { user_id: 2, award_id: 2, status: "Supervisor Review", drafting: false },
-    { user_id: 2, award_id: 3, status: "Drafting", drafting: true },
+    { user_id: 2, award_id: 3, status: "Drafting", drafting: false },
     { user_id: 2, award_id: 4, status: "Supervisor Review", drafting: false },
 
     { user_id: 3, award_id: 1, status: "Supervisor Review", drafting: false },
-    { user_id: 3, award_id: 2, status: "Drafting", drafting: true },
+    { user_id: 3, award_id: 2, status: "Drafting", drafting: false },
     { user_id: 3, award_id: 4, status: "Supervisor Review", drafting: false },
 
-    { user_id: 4, award_id: 1, status: "Drafting", drafting: true },
-    { user_id: 4, award_id: 2, status: "Drafting", drafting: true },
-    { user_id: 4, award_id: 3, status: "Drafting", drafting: true },
+    { user_id: 4, award_id: 1, status: "Drafting", drafting: false },
+    { user_id: 4, award_id: 2, status: "Drafting", drafting: false },
+    { user_id: 4, award_id: 3, status: "Drafting", drafting: false },
     { user_id: 4, award_id: 4, status: "Supervisor Review", drafting: false },
 
     { user_id: 5, award_id: 1, status: "Supervisor Review", drafting: false },
