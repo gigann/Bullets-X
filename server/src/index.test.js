d816faacd6ece63d230d19f054560612faf8124d
gigann <issacgann@gmail.com>
2025-04-14 22:43:17 +0200

    fixed server tests and added tests for award endpoints

--- server/src/index.test.js
+++ server/src/index.test.js
@@ -48,13 +48,15 @@ describe('activity routes', () => {
       })
   })
 
-  test('POST /activity/ should return JSON content and CREATED (201).', (done) => {
+  test('POST /activity should return JSON content and CREATED (201).', (done) => {
     request(app)
-      .post('/activity/', JSON.stringify({
+      .post('/activity')
+    .send({
         user_id: 1,
         name: 'test name',
         description: 'test description'
-      }))
+    })
+      .set('Content-Type', 'application/json')
       .expect('Content-Type', /json/)
       .expect(201)
       .end((err, res) => {
@@ -63,15 +65,101 @@ describe('activity routes', () => {
       })
   })
 
-  test('DELETE /activity/:id should return JSON content and OK (200) or NOT FOUND (404).', (done) => {
+  test('PATCH /activity/:id should return JSON content and OK (200).', (done) => {
     request(app)
-      .delete('/activity/1')
+      .patch('/activity/1')
+    .send({
+        user_id: 1,
+        name: 'test name',
+        description: 'test description'
+    })
+      .set('Content-Type', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200 || 404)
+      .expect(200)
       .end((err, res) => {
         if (err) throw err;
         done();
       })
   })
 
+  test('DELETE /activity/:id should return JSON content and OK (200).', (done) => {
+    request(app)
+      .delete('/activity/1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      })
+  })
 })
+
+describe('award routes', () => {
+  test('GET /award should return JSON content and OK (200).', (done) => {
+    request(app)
+      .get('/award')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      })
+  })
+
+  test('GET /award/:id should return JSON content and OK (200).', (done) => {
+    request(app)
+      .get('/award/1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      })
+  })
+
+  test('POST /award should return JSON content and CREATED (201).', (done) => {
+    request(app)
+      .post('/award')
+      .send({
+        name: 'test name',
+        description: 'test description',
+        due_date: new Date(),
+        bullet_minimum: 0,
+        bullet_maximum: 1
+      })
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      })
+  })
+
+  test('PATCH /award/:id should return JSON content and OK (200).', (done) => {
+    request(app)
+      .patch('/award/1')
+      .send({
+        name: 'test name',
+        description: 'test description'
+      })
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      })
+  })
+  test('DELETE /award/:id should return JSON content and OK (200).', (done) => {
+    request(app)
+      .delete('/award/1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      })
+  })
+})
+
