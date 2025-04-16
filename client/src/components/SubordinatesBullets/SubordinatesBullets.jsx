4b87faddcdb2489430a339e99678e40474ea9b7c
ashdrey1110 <ashrey1110@gmail.com>
2025-04-16 14:27:06 -0700

    updated sub bullets -- no window refresh

--- client/src/components/SubordinatesBullets/SubordinatesBullets.jsx
+++ client/src/components/SubordinatesBullets/SubordinatesBullets.jsx
@@ -2,8 +2,7 @@ import "./SubordinatesBullets.css";
 import { useState, useEffect, useContext } from "react";
 import { useLocalStorage } from "@uidotdev/usehooks";
 import { useNavigate } from "react-router-dom";
-import SubordinateContext from '../Context/SubordinateContext';
-
+import SubordinateContext from "../Context/SubordinateContext";
 
 export default function SubordinatesBullets() {
   const [loading, setLoading] = useState(true);
@@ -20,18 +19,20 @@ export default function SubordinatesBullets() {
   const [impact, setImpact] = useState("");
   const [result, setResult] = useState("");
   const [loggedIn, setLoggedIn] = useLocalStorage("loggedIn");
-  const {subordinateInfo, setSubordinateInfo} = useContext(SubordinateContext)
+  const { subordinateInfo, setSubordinateInfo } =
+    useContext(SubordinateContext);
   const navigate = useNavigate();
-  const [certainAward, setCertainAward] = useLocalStorage('certainAward')
-  const [certainSubordinateID, setCertainSubordinateID] = useLocalStorage('certainSubordinateID')
-  const [revisedHidden, setRevisedHidden] = useState(true)
-
+  const [certainAward, setCertainAward] = useLocalStorage("certainAward");
+  const [certainSubordinateID, setCertainSubordinateID] = useLocalStorage(
+    "certainSubordinateID"
+  );
+  const [revisedHidden, setRevisedHidden] = useState(true);
 
   const userID = loggedIn?.id;
 
   const handleSetSubordinateID = (id, bulletName, awardID) => {
     // setSubordinateID(id);
-    setName(bulletName)
+    setName(bulletName);
     // setAwardID(awardID)
     // setMakeFormVisible(true)
     console.log("Selected Subordinate ID:", id);
@@ -42,40 +43,49 @@ export default function SubordinatesBullets() {
     navigate(-1);
   };
 
-  useEffect(() => {
-    fetch(`http://localhost:3001/bullet/completed/${certainSubordinateID}/${certainAward}`)
-      .then((res) =>  {
+  const fetchBullets = () => {
+    fetch(
+      `http://localhost:3001/bullet/completed/${certainSubordinateID}/${certainAward}`
+    )
+      .then((res) => {
         if (!res.ok) {
-        throw new Error("No bullets found for this user and award.");
-      }
-      return res.json();
-    })
-      .then(data => { setSubordinateInfo(data)})
-  }, [certainSubordinateID, certainAward])
+          throw new Error("No bullets found for this user and award.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setSubordinateInfo(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchBullets();
+  }, [certainSubordinateID, certainAward]);
 
   const handleChangeStatus = (bulletID) => {
     fetch(`http://localhost:3001/bullet/${bulletID}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      status: "Supervisor Approved",
-      drafting: false,
-    }),
-  })
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error("Failed to update status");
-    }
-    return res.json();
-  })
-  .catch((error) => {
-    console.error("Error updating status:", error.message);
-  });
-};
-
-
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        status: "Supervisor Approved",
+        drafting: false,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to update status");
+        }
+        return res.json();
+      })
+      .then(() => {
+        fetchBullets();
+      })
+      .catch((error) => {
+        console.error("Error updating status:", error.message);
+      });
+  };
 
   const handleAddBullet = () => {
     const emptyFieldsCheck = !action.trim() && !impact.trim() && !result.trim();
@@ -100,19 +110,22 @@ export default function SubordinatesBullets() {
         award_id: certainAward,
       }),
     })
-    .then((res) => res.json())
-    .then(() => {
-      const awardNamesPromises = subordinateAwards.map((awardInfo) =>
-      fetch(`http://localhost:3001/bullet/users/${subordinateID}`)
       .then((res) => res.json())
-      .then((data) => {
-            setSubordinateBullets(data);
-          }));
+      .then(() => {
+        // const awardNamesPromises = subordinateAwards.map((awardInfo) =>
+        //   fetch(`http://localhost:3001/bullet/users/${subordinateID}`)
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //       setSubordinateBullets(data);
+        //     })
+        // );
+        fetchBullets();
         setName("");
         setAction("");
         setImpact("");
         setResult("");
-        setRevisedHidden(!revisedHidden)
+        setRevisedHidden(!revisedHidden);
+        
       })
       .catch((err) => {
         console.log(err);
@@ -126,9 +139,11 @@ export default function SubordinatesBullets() {
   //   return <p>You have no subordinates</p>;
 
   useEffect(() => {
-    if (!subordinateInfo || (Array.isArray(subordinateInfo) && subordinateInfo.length === 0)) {
+    if (
+      !subordinateInfo ||
+      (Array.isArray(subordinateInfo) && subordinateInfo.length === 0)
+    ) {
       navigate(`/subordinates/${userID}`);
-
     }
   }, [subordinateInfo, userID, navigate]);
 
@@ -136,104 +151,134 @@ export default function SubordinatesBullets() {
     <>
       {/* <button onClick={() => console.log()}>Console.log</button> */}
       <div className="subordinates-bullets-page-container">
-          <div className={!revisedHidden ? "subordinate-bullet-card" : "" } hidden={revisedHidden}>
+        <div
+          className={!revisedHidden ? "subordinate-bullet-card" : ""}
+          hidden={revisedHidden}
+        >
+          <h3>Add a Revised Bullet</h3>
 
-            <h3>Add a Revised Bullet</h3>
+          <h3>Name:</h3>
+          <input
+            className="name-suggestion"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            readOnly
+          />
+          <h3>Action:</h3>
 
-            <h3>Name:</h3>
-            <input
-              className = "name-suggestion"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              readOnly
-            />
-            <h3>Action:</h3>
+          <input
+            type="text"
+            value={action}
+            onChange={(e) => setAction(e.target.value)}
+          />
 
-            <input
-              type="text"
-              value={action}
-              onChange={(e) => setAction(e.target.value)}
-            />
+          <h3>Impact:</h3>
 
-            <h3>Impact:</h3>
+          <input
+            type="text"
+            value={impact}
+            onChange={(e) => setImpact(e.target.value)}
+          />
 
-            <input
-              type="text"
-              value={impact}
-              onChange={(e) => setImpact(e.target.value)}
-            />
+          <h3>Result:</h3>
+          <input
+            type="text"
+            value={result}
+            onChange={(e) => setResult(e.target.value)}
+          />
 
-            <h3>Result:</h3>
-            <input
-              type="text"
-              value={result}
-              onChange={(e) => setResult(e.target.value)}
-            />
+          <button onClick={handleAddBullet}>Add Bullet</button>
+          {/* <button onClick={() => setMakeFormVisible(false)}>Cancel</button> */}
+          <button
+            hidden={revisedHidden}
+            onClick={() => {
+              setRevisedHidden(!revisedHidden);
+            }}
+            className="bullets-xbutton"
+          >
+            X
+          </button>
+        </div>
 
-            <button onClick={handleAddBullet}>Add Bullet</button>
-            {/* <button onClick={() => setMakeFormVisible(false)}>Cancel</button> */}
-            <button hidden={revisedHidden} onClick={() => {setRevisedHidden(!revisedHidden)}} className="bullets-xbutton">X</button>
-          </div>
+        {!subordinateInfo ||
+        (Array.isArray(subordinateInfo) && subordinateInfo.length === 0) ? (
+          <p className="no-bullets-message">
+            No completed bullets for this subordinate
+          </p>
+        ) : (
+          subordinateInfo.map((bu, i) => (
+            <div key={i} className="subordinate-bullet-card-ethan">
+              <p className="subordinate-bullet-title">{bu.name}</p>
 
-      {!subordinateInfo || (Array.isArray(subordinateInfo) && subordinateInfo.length === 0) ? (
-        <p className = "no-bullets-message">No completed bullets for this subordinate</p>
-      ) : (
-        subordinateInfo.map((bu, i) => (
-          <div key={i} className="subordinate-bullet-card-ethan">
-            <p className="subordinate-bullet-title">
-              {bu.name}
-            </p>
-            
-              
-              
-            
-            <p className="subordinate-bullet-section">
-              <span className="subordinate-bullet-label" style={{ color: "#B67EF0" }}>{bu.status}</span>{" "}
-              <br></br>
-              <span className="subordinate-bullet-label" style={{ color: "#B67EF0" }}>{new Date(bu.updated_at).toLocaleDateString()} -{" "}
-              {new Date(bu.updated_at).toLocaleTimeString()}</span>{" "}
-              
-            </p>
-            <p className="subordinate-bullet-section">
-              <span className="subordinate-bullet-label">Action:</span>{" "}
-              {bu.action}
-            </p>
-            <p className="subordinate-bullet-section">
-              <span className="subordinate-bullet-label">Impact:</span>{" "}
-              {bu.impact}
-            </p>
-            <p className="subordinate-bullet-section">
-              <span className="subordinate-bullet-label">Result:</span>{" "}
-              {bu.result}
-            </p>
-            <button
-              className="suggest"
-              onClick={() => {
-                handleSetSubordinateID(bu.user_id, bu.award_name, bu.award_id);
-                setName(bu.name)
-                setAction(bu.action)
-                setImpact(bu.impact)
-                setResult(bu.result)
-                setRevisedHidden(!revisedHidden)
-              }}>Submit Revision</button>
-            {bu.status != "Supervisor Approved" ? (
-        <button
-        className = "approve"
-        style={{ background: "green" }}
-        onClick ={() => {
-          handleChangeStatus(bu.id);
-          // navigate(`/subordinates/${userID}`)
-          window.location.reload()
-        }}>Approve</button>
-      ) : (
-        <h3 style={{ color: "green" }}>Supervisor Approved!</h3>
-      )}
-          </div>
-        ))
-      )}
+              <p className="subordinate-bullet-section">
+                <span
+                  className="subordinate-bullet-label"
+                  style={{ color: "#B67EF0" }}
+                >
+                  {bu.status}
+                </span>{" "}
+                <br></br>
+                <span
+                  className="subordinate-bullet-label"
+                  style={{ color: "#B67EF0" }}
+                >
+                  {new Date(bu.updated_at).toLocaleDateString()} -{" "}
+                  {new Date(bu.updated_at).toLocaleTimeString()}
+                </span>{" "}
+              </p>
+              <p className="subordinate-bullet-section">
+                <span className="subordinate-bullet-label">Action:</span>{" "}
+                {bu.action}
+              </p>
+              <p className="subordinate-bullet-section">
+                <span className="subordinate-bullet-label">Impact:</span>{" "}
+                {bu.impact}
+              </p>
+              <p className="subordinate-bullet-section">
+                <span className="subordinate-bullet-label">Result:</span>{" "}
+                {bu.result}
+              </p>
+
+              {bu.status != "Supervisor Approved" ? (
+                <>
+                  <button
+                    className="suggest"
+                    onClick={() => {
+                      handleSetSubordinateID(
+                        bu.user_id,
+                        bu.award_name,
+                        bu.award_id
+                      );
+                      setName(bu.name);
+                      setAction(bu.action);
+                      setImpact(bu.impact);
+                      setResult(bu.result);
+                      setRevisedHidden(!revisedHidden);
+                    }}
+                  >
+                    Submit Revision
+                  </button>
+                  <button
+                    className="approve"
+                    style={{ background: "green" }}
+                    onClick={() => {
+                      handleChangeStatus(bu.id);
+                      // navigate(`/subordinates/${userID}`)
+                      // window.location.reload();
+                    }}
+                  >
+                    Approve
+                  </button>
+                </>
+              ) : (
+                <h3 style={{ color: "green" }}>Supervisor Approved!</h3>
+              )}
+            </div>
+          ))
+        )}
         {/* <button onClick={() => setRevisedHidden(!revisedHidden)}>Add Revised Bullet</button> */}
-      <button onClick={backButton}>Back</button>
+        <button onClick={backButton}>Back</button>
       </div>
     </>
   );
